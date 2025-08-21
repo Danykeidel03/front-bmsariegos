c5f7b34fd0213e1e1dec964e793dddf0bbf3dd83
danielfernandez <danielfernandez@craftelier.com>
2025-08-21 14:24:32 +0200

    api key

--- src/services/apiBirthday.js
+++ src/services/apiBirthday.js
@@ -5,7 +5,10 @@ const URL_API = 'https://back-bmsariegos-production.up.railway.app';
 const api = axios.create({
     baseURL: URL_API,
     timeout: 10000,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'x-api-key': 'bm-sariegos-internal-2024'
+    }
 })
 
 const objServices = {
